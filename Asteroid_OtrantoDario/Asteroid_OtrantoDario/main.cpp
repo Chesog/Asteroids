61b405116737e4ef6f116a36c25ddfa5fc9f104c
Cheso <chesomartines@gmail.com>
2022-09-13 12:05:45 -0300

    init window

---
+++ Asteroid_OtrantoDario/Asteroid_OtrantoDario/main.cpp
@@ -0,0 +1,31 @@
+
+#include "raylib.h"
+
+void main()
+{
+  
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);
+
+
+    while (!WindowShouldClose())   
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+
+
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Congrats! You created your first window!", 190, 200, 20, BLACK);
+
+        EndDrawing();
+    }
+
+    CloseWindow();       
+}
